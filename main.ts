69ec9197b5c1acfabe6c54e6972faa52973c6a6d
Josjeroobol <124696948+Josjeroobol@users.noreply.github.com>
2023-04-25 15:49:55 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let ir_waarde = 0
+let rechts = 4
+let links = 3
+let vooruit = 1
+let achteruit = 2
+basic.forever(function () {
+    let stop = 0
+    if (ir_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
+})
